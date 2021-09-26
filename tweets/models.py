d13c7cdad192e04e2c85d15ce0a59c27e0b7204c
1ypen <fdokpy@gmail.com>
2021-09-27 01:22:41 +0600

    removed the "get_formatted_creation_date" function from the tweets model

--- tweets/models.py
+++ tweets/models.py
@@ -16,44 +16,10 @@ class Tweets(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     users_like = models.ManyToManyField(User, related_name='tweets_liked', blank=True)
-
     visibility = models.CharField(max_length=55, choices=VISIBILITY_CHOICES, default='all')
 
     class Meta:
         ordering = ('-created_date',)
-
-    @property
-    def get_formatted_creation_date(self):
-        """
-        returns the formatted date of creation of the tweet in the form
-        1m if the tweet was created a minute ago
-        1h if the tweet was created an hour ago etc
-        """
-        time_now = datetime.now(timezone.utc)
-        time_period = abs(self.created_date - time_now)
-
-        hours, remainder = divmod(time_period.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-
-        if time_period.days >= 365:
-            return self.created_date.strftime('%b %-d, %Y')
-
-        if time_period.days >= 1:
-            return self.created_date.strftime('%b %-d')
-
-        if 1 <= hours <= 24:
-            return self.created_date.strftime('%-Hh')
-
-        if minutes:
-            return f'{minutes}m'
-
-        if seconds:
-            return f'{seconds}s'
-
-        return '0s'
-
-    def get_like_count(self):
-        return self.users_like.all().count()
 
     def __str__(self):
         return self.text[:50]
