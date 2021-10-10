90d96e3fb6ec07ec655e604e3de11b961cc59053
1ypen <fdokpy@gmail.com>
2021-10-10 16:11:10 +0600

    Added ability to send mail 📨 #2

--- Twitter/settings.py
+++ Twitter/settings.py
@@ -100,6 +100,9 @@ DATABASES = {
     }
 }
 
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -167,5 +170,19 @@ REST_FRAMEWORK = {
     ),
 }
 
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'twitter.verify.clone@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'twitter.verify.clone@gmail.com'
+SITE_NAME = "Twitter"
+
+
+DJOSER = {
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
+    'SERIALIZERS': {'token_create': 'accounts.serializers.CustomTokenCreateSerializer'}
+}
+
