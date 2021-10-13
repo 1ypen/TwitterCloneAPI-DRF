a14f8c48c871155b6a0ef17047cf389108ddd233
1ypen <fdokpy@gmail.com>
2021-10-13 12:31:40 +0600

    🔧 improved the database query

--- tweet/serializers.py
+++ tweet/serializers.py
@@ -1,4 +1,3 @@
-from django.db.models import Count, Case, When, F
 from rest_framework import serializers
 
 from .models import Tweet, TweetImage
--- tweet/views.py
+++ tweet/views.py
@@ -1,11 +1,11 @@
-from django.db.models import Count, F, Case, When
+from django.db.models import Count, F, Case, When, QuerySet, Subquery, OuterRef, Prefetch
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, renderers
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.views import APIView, Response
 
-from .models import Tweet
+from .models import Tweet, TweetImage
 from .serializers import TweetSerializer, TweetCreateSerializer
 from .utils import merge_values, get_object_or_none
 
@@ -21,8 +21,8 @@ class TweetListApiView(ListCreateAPIView):
     def get_queryset(self):
 
         # i get a list of IDs of tweets that the user liked
-        user_likes_id = self.request.user.tweets_liked.values_list('id', flat=True)
-
+        # user_likes_id = self.request.user.tweets_liked.values_list('id', flat=True)
+        user_likes_id = []
         # i make a query to the database to get responses
         # with additional fields that are not in the tweet model,
         # use the select_related, prefetch_related and values methods to optimize the query
@@ -40,7 +40,18 @@ class TweetListApiView(ListCreateAPIView):
                 user_avatar=F('user__avatar')
             ).order_by('-id')
 
-        return merge_values(queryset)
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = merge_values(self.filter_queryset(self.get_queryset()))
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
@@ -56,21 +67,21 @@ class TweetDetailApiView(RetrieveAPIView):
         queryset = Tweet.objects \
             .select_related('user') \
             .prefetch_related('users_like') \
-            .prefetch_related('images')
+            .prefetch_related('images'). \
+            values(
+                'id', 'created_date', 'text',
+                like_count=Count('users_like'),
+                img=F('images__img'),
+                is_liked=Case(When(id__in=[], then=True), default=False),
+                user_name=F('user__name'),
+                user_login=F('user__login'),
+                user_avatar=F('user__avatar')
+            )
 
         return queryset
 
     def get_object(self):
-        user_likes_id = self.request.user.tweets_liked.values_list('id', flat=True)
-        queryset = self.filter_queryset(self.get_queryset().values(
-            'id', 'created_date', 'text',
-            like_count=Count('users_like'),
-            img=F('images__img'),
-            is_liked=Case(When(id__in=user_likes_id, then=True), default=False),
-            user_name=F('user__name'),
-            user_login=F('user__login'),
-            user_avatar=F('user__avatar')
-        ).filter(**self.kwargs))
+        queryset = self.filter_queryset(self.get_queryset().filter(**self.kwargs))
 
         obj = merge_values(queryset)
         if not obj:
